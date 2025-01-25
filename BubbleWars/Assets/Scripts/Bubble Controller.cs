5add4db9a8332b8185347ae72aab2f9ac6dcaf36
Joshua Dunn <89932329+JoshDunn88@users.noreply.github.com>
2025-01-25 12:56:30 -0500

    bubble ttl

--- BubbleWars/Assets/Scripts/Bubble Controller.cs
+++ BubbleWars/Assets/Scripts/Bubble Controller.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class BubbleController : MonoBehaviour
 {
+    public float timetolive;
+    private float birthtime;
+    private float deathtime;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        birthtime = Time.time;
+        deathtime = birthtime + timetolive;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > deathtime)
+        {
+            Destroy(gameObject);
+        }
     }
 }
