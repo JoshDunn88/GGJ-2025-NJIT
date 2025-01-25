f4e38103275574ed55c133c501a278c37d673a71
Adira P Samaroo <aps66@njit.edu>
2025-01-25 14:52:14 -0500

    UI Progging

--- BubbleWars/Assets/Scripts/GameManager.cs
+++ BubbleWars/Assets/Scripts/GameManager.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+	public GameObject[] scoreGrids;
+	public GameObject scorePrefab;
+
+	public GameObject gamePanel;
+	public GameObject menuPanel;
+
+	public static GameManager Instance { get; private set; }
+
+    private void Awake()
     {
-        
+        if (Instance != null)
+        {
+			Debug.LogError("More than one GameManager instance!");
+        }
+		Instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+	public void PlayGame()
+	{
+		menuPanel.SetActive(false);
+
+		gamePanel.SetActive(true);
+	}
+	
+	public void QuitGame()
     {
-        
-    }
+		Application.Quit();
+		Debug.Log("Application is exiting");
+	}
+
+	// Add score indicator to the scoring player
+    public void AddScore(int player)
+	{
+		GameObject point = Instantiate(scorePrefab, transform.position, Quaternion.identity) as GameObject;
+		point.transform.SetParent(scoreGrids[player].transform);
+	}
 }
