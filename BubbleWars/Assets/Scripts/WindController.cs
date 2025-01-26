227e49fadd7351424ddcc9273213a29018e3728d
Joshua Dunn <89932329+JoshDunn88@users.noreply.github.com>
2025-01-25 21:11:25 -0500

    wind display + fixes

--- BubbleWars/Assets/Scripts/WindController.cs
+++ BubbleWars/Assets/Scripts/WindController.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class WindController : MonoBehaviour
 {
+    public GameObject windArrow;
+    public GameObject windText;
     public float changeFrequency;
     public float maxSpeed;
+    public float minSpeed;
 
     public float direction;
     public float magnitude;
@@ -14,18 +18,24 @@ public class WindController : MonoBehaviour
     private float targetDirection;
     private float targetMagnitude;
     private float lastChange;
-    private float magnitudeChangeTime = 5;
-    private float directionChangeTime = 2;
+    private float magnitudeChangeTime = 20;
+    private float directionChangeTime = 20;
 
     public Vector2 getWindVector()
     {
         Vector2 wind = new Vector2 (0, 0);
-        wind.x = Mathf.Cos(direction) * magnitude;
-        wind.y = Mathf.Sin(direction) * magnitude;
+        wind.x = Mathf.Cos(direction * Mathf.Deg2Rad) * magnitude;
+        wind.y = Mathf.Sin(direction * Mathf.Deg2Rad) * magnitude;
 
         return wind;
     }
 
+    private Quaternion getWindRotation()
+    {
+        
+        Quaternion newRot = Quaternion.Euler(0f, 0f, direction-90);
+        return newRot;
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -40,9 +50,12 @@ public class WindController : MonoBehaviour
         if (Time.time > lastChange + changeFrequency)
         {
             targetDirection = Random.Range(0, 360);
-            targetMagnitude = Random.Range(0, maxSpeed/1000);
+            targetMagnitude = Random.Range(minSpeed/1000, maxSpeed/1000);
             lastChange = Time.time;
         }
+        //
+        windArrow.GetComponent<RectTransform>().rotation = getWindRotation();
+        windText.GetComponent<TMP_Text>().text = (Mathf.Floor(magnitude * 10000) / 10 + " MPH");
     }
 
     private void FixedUpdate()
